848a0e4026742664d87fe7ca0418c0ec3aaac4fe
Taylor <tbnelson01@gmail.com>
2023-01-25 16:53:12 -0500

    basic styling on screen, needs scaling

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -6,32 +6,50 @@ export default function HomeScreen() {
     const [smallRed, setSmallRed] = useState('#db0000')
     const [smallYellow, setSmallYellow] = useState('#f0cc00')
     const [smallGreen, setSmallGreen] = useState('limegreen')
-    
+
     return (
         <View style={styles.container}>
             <View style={styles.topBorder}>
-            <View style={styles.topLeft}/>
-            <View style={styles.topDiagonal}>
-                <View style={styles.diagonal}/>
-            </View>
-            <View style={styles.topRight}/>
+                <View style={styles.topLeft} />
+                <View style={styles.topDiagonal}>
+                    <View style={styles.diagonal} />
+                </View>
+                <View style={styles.topRight} />
             </View>
             <View style={styles.blueLightBorder} />
-            <View style={[styles.blueLight, {backgroundColor: blueLight}]} />
+            <View style={[styles.blueLight, { backgroundColor: blueLight }]} />
             <View style={styles.blueLightShine} />
             <View style={styles.smallLightContainer}>
-                <View style={[styles.smallLight, {backgroundColor: smallRed}]}/>
-                <View style={[styles.smallLight, {backgroundColor: smallYellow}]} />
-                <View style={[styles.smallLight, {backgroundColor: smallGreen}]} />
+                <View style={[styles.smallLight, { backgroundColor: smallRed }]} />
+                <View style={[styles.smallLight, { backgroundColor: smallYellow }]} />
+                <View style={[styles.smallLight, { backgroundColor: smallGreen }]} />
             </View>
             <View style={styles.screenContainer}>
+                <View style={styles.topLines}/>
+                <View style={styles.botLines}/>
+                <View style={styles.screenLight} />
                 <View style={styles.screenCorner} />
                 <View style={styles.screenCornerBorder} />
                 <View style={styles.screen}>
-                    <Image style={styles.sprite} source={{ uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/145.png"}}/>
+                    <Image style={styles.sprite} source={{ uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/145.png" }} />
                 </View>
+                <View style={styles.leftScreenLight}/>
+                <View style={styles.rightScreenLight}/>
             </View>
 
+
+            <View style={styles.bottomDex}>
+                <View style={styles.directionalPad}>
+                    <View style={styles.upDirection} />
+                    <View style={styles.downDirection} />
+                    <View style={styles.leftDirection} />
+                    <View style={styles.rightDirection} />
+                </View>
+                <View style={styles.thinRed} />
+                <View style={styles.thinBlue} />
+                <View style={styles.blackButton} />
+                <View style={styles.greenScreen} />
+            </View>
 
 
 
@@ -54,6 +72,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: "crimson",
     },
+    bottomDex: {
+        position: 'absolute',
+        top: 550,
+        width: '100%',
+        height: '35%'
+    },
+    blackButton: {
+        position: 'absolute',
+        top: 80,
+        left: 40,
+        backgroundColor: '#111',
+        borderWidth: 4,
+        borderRadius: 50,
+        height: 50,
+        width: 50,
+    },
     blueLight: {
         position: 'absolute',
         top: 60,
@@ -65,7 +99,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center"
     },
-    blueLightBorder :{
+    blueLightBorder: {
         position: 'absolute',
         top: 50,
         left: 20,
@@ -87,11 +121,82 @@ const styles = StyleSheet.create({
         width: 25,
         height: 25,
     },
+    directionalPad: {
+        position: 'absolute',
+        top: 80,
+        right: 20,
+        width: 150,
+        height: 150,
+    },
+    upDirection: {
+        width: 50,
+        height: 100,
+        backgroundColor: '#111',
+        position: 'absolute',
+        left: 50,
+        borderRadius: 10
+    },
+    downDirection: {
+        width: 50,
+        height: 100,
+        backgroundColor: '#111',
+        position: 'absolute',
+        left: 50,
+        bottom: 0,
+        borderRadius: 10
+    },
+    leftDirection: {
+        width: 100,
+        height: 50,
+        backgroundColor: '#111',
+        position: 'absolute',
+        borderRadius: 10,
+        top: 50
+    },
+    rightDirection: {
+        width: 100,
+        height: 50,
+        backgroundColor: '#111',
+        position: 'absolute',
+        borderRadius: 10,
+        top: 50,
+        right: 0
+    },
+    greenScreen: {
+        position: 'absolute',
+        top: 140,
+        left: 100,
+        backgroundColor: 'lime',
+        width: 145,
+        height: 70,
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    thinBlue: {
+        position: 'absolute',
+        top: 50,
+        left: 100,
+        width: 60,
+        height: 15,
+        backgroundColor: 'red',
+        borderRadius: 10,
+        borderWidth: 1,
+    },
+    thinRed: {
+        position: 'absolute',
+        top: 50,
+        left: 190,
+        width: 60,
+        height: 15,
+        backgroundColor: 'skyblue',
+        borderRadius: 10,
+        borderWidth: 1,
+    },
     smallLightContainer: {
-        width: 150, 
-        height: 50, 
-        position: 'absolute', 
-        top: 55, 
+        width: 150,
+        height: 50,
+        position: 'absolute',
+        top: 55,
         left: 110,
         flexDirection: 'row',
         justifyContent: 'space-evenly'
@@ -120,7 +225,7 @@ const styles = StyleSheet.create({
     },
     diagonal: {
         borderWidth: 2,
-        transform: [{rotate: "-29deg"}],
+        transform: [{ rotate: "-29deg" }],
         width: 95,
         marginLeft: -7,
     },
@@ -136,7 +241,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#efefef",
         borderWidth: 2,
         borderBottomLeftRadius: 4,
-        marginTop: -180,
+        position: 'absolute',
+        top: 200,
     },
     screenCorner: {
         position: "absolute",
@@ -160,7 +266,7 @@ const styles = StyleSheet.create({
         position: "absolute",
         top: 30,
         left: 30,
-        width: 260, 
+        width: 260,
         height: 260,
         backgroundColor: "#333",
         borderWidth: 2,
@@ -169,10 +275,59 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
+    screenLight: {
+        position: 'absolute',
+        top: 302,
+        left: 35,
+        width: 20,
+        height: 20,
+        borderRadius: 50,
+        backgroundColor: 'red',
+        borderWidth: 1,
+        zIndex: 1,
+    },
     sprite: {
         height: 200,
         width: 200,
-        resizeMode: 'cover', 
+        resizeMode: 'cover',
     },
-
+    topLines: {
+        position: 'absolute',
+        top: 300,
+        right: 30,
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+        width: 40,
+        height: 12,
+    },
+    botLines: {
+        position: 'absolute',
+        top: 320,
+        right: 30,
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+        width: 40,
+        height: 12,
+        zIndex: 3,
+    },
+    leftScreenLight: {
+        position: 'absolute',
+        top: 10,
+        left: 120,
+        backgroundColor: 'red',
+        width: 10,
+        height: 10,
+        borderRadius: 50,
+        borderWidth: 1
+    },
+    rightScreenLight: {
+        position: 'absolute',
+        top: 10,
+        right: 120,
+        backgroundColor: 'red',
+        width: 10,
+        height: 10,
+        borderRadius: 50,
+        borderWidth: 1
+    }
 });
